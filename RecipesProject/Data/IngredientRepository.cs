8874ed4b3820b9315507ef8e2244eda06ca5afb7
Diana-KDD <dianaklimovich44@gmail.com>
2026-05-28 11:56:12 +0300

    Add repository steps and ingredient

---
+++ RecipesProject/Data/IngredientRepository.cs
@@ -0,0 +1,31 @@
+﻿using RecipesProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesProject.Data
+{
+    public class IngredientRepository
+    {
+        private readonly DBContext _context;
+
+        public IngredientRepository(DBContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            _context.Ingredients.Add(ingredient);
+            _context.SaveChanges();
+        }
+
+        public void AddRange(List<Ingredient> ingredients)
+        {
+            _context.Ingredients.AddRange(ingredients);
+            _context.SaveChanges();
+        }
+    }
+}
